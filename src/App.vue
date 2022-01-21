b5a7683b6afa595147ee8bd5daca38674f536a31
kydner <mhd.rendika@gmail.com>
2022-01-21 23:07:35 +0700

    vietify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,86 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app id="inspire">
+    <v-app-bar app :elevation="0" clipped-left height="70" color="primary">
+      <!-- <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon> -->
+
+      <v-row>
+        <v-col>
+          <v-row>
+            <v-col>
+              <v-toolbar-title>
+                <v-img src="/MERDEKA_3D.svg"></v-img>
+              </v-toolbar-title>
+            </v-col>
+            <v-col cols="8">
+              <v-text-field
+                placeholder="Search"
+                filled
+                rounded
+                dense
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col> vvv </v-col>
+      </v-row>
+    </v-app-bar>
+
+    <v-navigation-drawer clipped app color="black">
+      <!--  -->
+      <v-list dense color="black">
+        <v-list-item-group v-model="selectedItem" color="primary">
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="main">
+      <v-container fluid>
+        <router-view />
+      </v-container>
+    </v-main>
+    <v-footer app height="200" color="primary">
+      <v-col class="text-center text-justify" cols="12">
+        © 1999-2012 21Cineplex.com. All materials and contents (texts, graphics,
+        and every attributes) of 21Cineplex or 21Cineplex.com website are
+        copyrights and trademarks of PT Nusantara Sejahtera Raya. Any commercial
+        usage of the materials and contents is forbidden without prior
+        permission from PT Nusantara Sejahtera Raya. There is no other
+        institutions/agencies outside PT Nusantara Sejahtera Raya allowed to use
+        www.21Cineplex.com (21Cineplex website) without prior permission from PT
+        Nusantara Sejahtera Raya
+      </v-col>
+    </v-footer>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    drawer: null,
+    selectedItem: 1,
+    items: [
+      { text: "Real-Time", icon: "mdi-clock" },
+      { text: "Audience", icon: "mdi-account" },
+      { text: "Conversions", icon: "mdi-flag" },
+    ],
+  }),
+};
+</script>
+<style scoped>
+.my-app.v-application .primary--text {
+  color: white !important;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.main {
+  background-color: #232c32;
 }
 </style>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#C0222E", // #E53935
+        dark: "#232C32", // #FFCDD2
+        black: "#191C1F",
+      },
+    },
+  },
+});
