1f41de3850b4a15374f7c8b2919d4e18f6f8ffb8
Rendhy 'k <rendika.debug@gmail.com>
2022-01-22 10:44:57 +0700

    fix responsive

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar app :elevation="0" clipped-left height="70" color="primary">
-      <!-- <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon> -->
-
-      <v-row>
-        <v-col>
+    <v-app-bar app :elevation="0" clipped-left color="primary">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-container class="py-0 fill-height" fluid>
+        <v-toolbar-title>
+          <v-img src="/MERDEKA_3D.svg" max-height="30" max-width="100"></v-img>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-text-field
+          dense
+          flat
+          hide-details
+          rounded
+          solo-inverted
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-responsive max-width="200">
           <v-row>
-            <v-col>
-              <v-toolbar-title>
-                <v-img src="/MERDEKA_3D.svg" height="50"></v-img>
-              </v-toolbar-title>
+            <v-col class="">
+              <v-img
+                src="/IMAX-logo.svg"
+                max-height="20"
+                max-width="50"
+              ></v-img>
             </v-col>
-            <v-col cols="8">
-              <v-text-field
-                placeholder="Search"
-                filled
-                rounded
-                dense
-              ></v-text-field>
+            <v-col class="">
+              <v-img
+                src="/Logo_Dolby_Atmos.svg"
+                max-height="20"
+                max-width="50"
+              ></v-img>
             </v-col>
           </v-row>
-        </v-col>
-        <v-col> vvv </v-col>
-      </v-row>
+        </v-responsive>
+      </v-container>
     </v-app-bar>
 
     <v-navigation-drawer clipped app color="black">
