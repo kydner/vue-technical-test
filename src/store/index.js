a18d8d05c20aee7c6d263b26e7ab170f6081f2bc
Rendhy 'k <rendika.debug@gmail.com>
2022-01-22 15:29:19 +0700

    add store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import search from "./search/index";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    search,
+  },
 });
